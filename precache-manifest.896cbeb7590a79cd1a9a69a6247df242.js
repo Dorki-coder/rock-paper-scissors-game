1540278432d9688a15dc5cbd66c8df4404307cc5
Dorki <bartdane@yandex.ru>
2022-09-30 16:44:20 +0600

    Updates

---
+++ precache-manifest.896cbeb7590a79cd1a9a69a6247df242.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ee60d0e1eba3bc6977",
+    "url": "/rock-paper-scissors-game/static/css/main.b1f28d38.chunk.css"
+  },
+  {
+    "revision": "b5ee60d0e1eba3bc6977",
+    "url": "/rock-paper-scissors-game/static/js/main.b5ee60d0.chunk.js"
+  },
+  {
+    "revision": "a22bbc484073c2ca57ef",
+    "url": "/rock-paper-scissors-game/static/js/1.a22bbc48.chunk.js"
+  },
+  {
+    "revision": "15279d5f6e3aa6e5f72d",
+    "url": "/rock-paper-scissors-game/static/js/runtime~main.15279d5f.js"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors-game/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors-game/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/rock-paper-scissors-game/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/rock-paper-scissors-game/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors-game/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "f4c973f743c8b75de9da23335e126dcd",
+    "url": "/rock-paper-scissors-game/static/media/logo-bonus.f4c973f7.svg"
+  },
+  {
+    "revision": "aced70522265bc609155bea44802776b",
+    "url": "/rock-paper-scissors-game/static/media/bg-pentagon.aced7052.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/rock-paper-scissors-game/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "d357f2dc1b21d32355069e26137c1baa",
+    "url": "/rock-paper-scissors-game/static/media/image-rules-bonus.d357f2dc.svg"
+  },
+  {
+    "revision": "b856dc8acd98d1a9ac29699daff37237",
+    "url": "/rock-paper-scissors-game/index.html"
+  }
+];
